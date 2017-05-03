5461631e732ac28983a3646aed83896d4e6b0bc8
Solcan Mihai <solcan.mihaiandrei@yahoo.com>
2017-05-03 12:20:58 +0300

    Added login file, done some routing

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-//Where to find the static ressources(aka html, js, css, images, etc)
-path.resolve(__dirname, '.../public');
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-	res.sendFile('welcome.html');
+	res.render('ejs/welcome.ejs');
+})
+
+app.get('/login', function(req, res){
+	res.render('ejs/login.ejs');
 })
 
 var server = app.listen(3000, function(){
